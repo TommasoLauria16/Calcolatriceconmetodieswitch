7bbfbac67c1cc404785630f105041b9b25ddb8ac
tommaso lauria <tommaso.lauria9@gmail.com>
2020-11-16 10:41:53 +0100

    Add project files.

---
+++ Calc1.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using  funzioni_matematiche_calc;
+
+
+namespace Calc1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = operazioniMatematiche.Numero();
+            double n2 = operazioniMatematiche.Numero();
+            double risultato = operazioniMatematiche.Operazioni(n1, n2);
+            Console.WriteLine($"il risultato dell'operazione scelta è {risultato}");
+        }
+    }
+}
---
+++ funzioni matematiche calc/operazioni matematiche.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace funzioni_matematiche_calc
+{
+    public class operazioniMatematiche
+    {
+        public static double Numero()
+        {
+            Console.Write("inserisci un numero: ");
+            double n = double.Parse(Console.ReadLine());
+            return n;
+        }
+        
+        public static double Operazioni(double n1, double n2)
+        {
+            double risultato=0;
+            Console.WriteLine("1=somma, 2=differenza, 3=moltiplicazione, 4=divisione ");
+            Console.WriteLine("seleziona l'operazione ");
+            string str = Console.ReadLine();
+            switch (str)
+            {
+                case "1":
+                case "somma":
+                    risultato = n1 + n2;
+                    break;
+                case "2":
+                case "differenza":
+                    risultato = n1 - n2;
+                    break;
+                case "3":
+                case "moltiplicazione":
+                    risultato = n1 * n2;
+                    break;
+                case "4":
+                case "divisione":
+                    risultato = n1 / n2;
+                    break;                
+            }
+            return risultato;
+        }
+    }
+}
